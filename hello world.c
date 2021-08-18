b815035188a1c4f0a52fce4fb4a29323fd199e3f
Roshan-Darnal <89129290+Roshan-Darnal@users.noreply.github.com>
2021-08-18 14:03:36 +0530

    Create hello world.c

---
+++ hello world.c
@@ -0,0 +1,19 @@
+# hello world
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter the number of elements to be stored in the array \n");
+    scanf("%d",&n);
+    int arr[n];
+    printf("enter the elements of the array\n");
+    for(int i=0;i<n;i++)
+    {
+        scanf("\n%d",&arr[i]);
+    }
+    printf("the elements in the array is: \n");
+    for(int j=0;j<n;j++)
+    {
+        printf("%d\n",arr[j]);
+    }
+}
